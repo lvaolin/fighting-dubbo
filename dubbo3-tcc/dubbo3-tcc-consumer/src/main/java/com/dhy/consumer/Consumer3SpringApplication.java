c55d5e853dca1ef1be9f709e78d8ec132ccc8205
lvaolin <lvaolin@foresee.com.cn>
2022-04-20 10:52:19 +0800

    test

--- dubbo3-tcc/dubbo3-tcc-consumer/src/main/java/com/dhy/consumer/Consumer3SpringApplication.java
+++ dubbo3-tcc/dubbo3-tcc-consumer/src/main/java/com/dhy/consumer/Consumer3SpringApplication.java
@@ -1,6 +1,5 @@
 package com.dhy.consumer;
 
-import org.apache.dubbo.common.constants.CommonConstants;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,7 +8,6 @@ public class Consumer3SpringApplication {
 
     public static void main(String[] args) {
         System.setProperty("dubbo.application.logger","log4j2");
-        //System.setProperty(CommonConstants.ENABLE_NATIVE_JAVA_GENERIC_SERIALIZE,"true");
         SpringApplication.run(Consumer3SpringApplication.class, args);
     }
 
--- dubbo3-tcc/dubbo3-tcc-provider/src/main/java/com/dhy/provider/Provider3SpringApplication.java
+++ dubbo3-tcc/dubbo3-tcc-provider/src/main/java/com/dhy/provider/Provider3SpringApplication.java
@@ -1,6 +1,5 @@
 package com.dhy.provider;
 
-import org.apache.dubbo.common.constants.CommonConstants;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -10,7 +9,6 @@ public class Provider3SpringApplication {
 
     public static void main(String[] args) {
         System.setProperty("dubbo.application.logger","log4j2");
-        //System.setProperty(CommonConstants.ENABLE_NATIVE_JAVA_GENERIC_SERIALIZE,"true");
         ConfigurableApplicationContext run = SpringApplication.run(Provider3SpringApplication.class, args);
         System.out.println("-----------");
     }
